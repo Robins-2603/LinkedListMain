894ff2cded6bce0bb3bf9ffa20c3ddf0322ddf68
Robins-2603 <robinsforwork@gmail.com>
2022-07-02 16:15:05 +0530

    Added: welcome message

---
+++ src/com/linkedList/LinkedListMain.java
@@ -0,0 +1,9 @@
+package com.linkedList;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("-------welcome to linkedList---------");
+    }
+
+
+}
